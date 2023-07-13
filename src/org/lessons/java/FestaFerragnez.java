393c6f30e03fe5455a6f731d6c9f54e18e261211
Ales <alocatelli798@gmail.com>
2023-07-13 15:03:25 +0200

    complete without bonus

---
+++ src/org/lessons/java/FestaFerragnez.java
@@ -0,0 +1,63 @@
+package org.lessons.java;
+
+//creare e inizializzare array contenente nomi invitati
+//chiedere il nome all'utente
+//verificare che il nome sia presente nella lista con metodo .equals()
+//comunicare all'utente se può entrare o meno
+//usare il ciclo for e poi ciclo while come bonus (o viceversa)
+
+
+import java.util.Scanner;
+
+public class FestaFerragnez {
+
+
+    public static void main(String[] args) {
+
+    //creo una array stringa con all'interno i nomi degli invitati
+        String[] invitatiFesta = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti",
+                "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        // chiedo il nome all'utente tramite input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci qui il tuo nome: ");
+        String name = sc.nextLine();
+
+        //verifico se il nome dell'utente è presente nella lista
+        //e comunico se può entrare o meno
+
+        /*BONUS: ciclo for
+        boolean found = false;
+
+        for(int i = 0; i < invitatiFesta.length; i++) {
+
+
+            if (invitatiFesta[i].equals(name)) {
+
+                found = true;
+                System.out.println("Il tuo nome è presente nella lista. Puoi entrare!");
+                 break;
+            }
+
+        }
+
+        if(!found)
+        {
+            System.out.println("Mi dispiace, il tuo nome non è presente nella lista");
+
+        }*/
+
+        //ciclo while
+
+
+
+
+
+        //chiudo lo scanner
+        sc.close();
+
+
+    }
+
+
+}
